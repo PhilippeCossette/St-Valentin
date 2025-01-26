4881886089337759ddaf05a3cd9a3f2ca074726d
Philippe Cossette <philcossette@hotmail.com>
2025-01-26 01:13:54 -0500

    initilsation

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const letter = document.querySelector(".letter");
+const pull = document.querySelector(".pull");
+let openState = "closed";
+console.log(openState);
+
+pull.addEventListener("click", () => {
+  if (openState == "closed") {
+    letter.style.transform = "translateY(20%)";
+    openState = "opened";
+  } else {
+    letter.style.transform = "translateY(100%)";
+    openState = "closed";
+  }
+});
