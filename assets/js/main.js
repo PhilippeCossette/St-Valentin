58b0e1242a59b7d1fd15975bb16c002ab7e0458a
Philippe Cossette <philcossette@hotmail.com>
2025-02-05 01:00:40 -0500

    fixed letter transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ console.log(openState);
 
 pull.addEventListener("click", () => {
   if (openState == "closed") {
-    letter.style.transform = "translateY(20%)";
+    letter.style.transform = "translateY(5%)";
     openState = "opened";
   } else {
     letter.style.transform = "translateY(100%)";
